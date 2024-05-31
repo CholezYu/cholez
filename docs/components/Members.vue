17ab520fccd2d415510654b14cf291c256f29df9
CholezYu <galaxy.nebula2021@gmail.com>
2024-06-01 01:27:45 +0800

    chore(主页): update tagline & 团队成员组件抽离

---
+++ docs/components/Members.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+  import { VPTeamPage, VPTeamPageTitle, VPTeamMembers } from "vitepress/theme"
+  
+  const members = [
+    {
+      avatar: "https://www.github.com/CholezYu.png",
+      name: "Cholez.Yu",
+      title: "Front-end King",
+      desc: "🌈 仰望星空的人，不应该被嘲笑",
+      links: [
+        { icon: "github", link: "https://github.com/CholezYu" },
+        {
+          icon: { svg: `<svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path d="M937.395 423.91a387.61 387.61 0 0 1-232.883-77.824v352.308c0 179.942-138.624 325.606-309.606 325.606-171.008 0-309.607-145.69-309.607-325.606 0-179.892 138.599-325.581 309.607-325.581 17.1 0 33.69 1.485 49.894 4.275v186.624a132.787 132.787 0 0 0-48.589-9.216c-76.288 0-138.214 65.024-138.214 145.306 0 80.204 61.9 145.305 138.214 145.305 76.186 0 138.087-65.1 138.087-145.305V0h172.697c0 134.502 103.68 243.507 231.603 243.507v180.301l-1.203 0.102z" /></svg>` },
+          link: "https://v.douyin.com/i2THwY7e"
+        }
+      ]
+    },
+    {
+      avatar: "https://p3-pc.douyinpic.com/img/aweme-avatar/tos-cn-i-0813_ooI98CkABJNjr6oonAEeWqeCLAAC0gbODHAAzz~c5_300x300.jpeg?from=2956013662",
+      name: "小丸子",
+      title: "清清不开心",
+      desc: "FE Developer",
+      links: [
+        {
+          icon: { svg: `<svg class="icon" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="200" height="200"><path d="M937.395 423.91a387.61 387.61 0 0 1-232.883-77.824v352.308c0 179.942-138.624 325.606-309.606 325.606-171.008 0-309.607-145.69-309.607-325.606 0-179.892 138.599-325.581 309.607-325.581 17.1 0 33.69 1.485 49.894 4.275v186.624a132.787 132.787 0 0 0-48.589-9.216c-76.288 0-138.214 65.024-138.214 145.306 0 80.204 61.9 145.305 138.214 145.305 76.186 0 138.087-65.1 138.087-145.305V0h172.697c0 134.502 103.68 243.507 231.603 243.507v180.301l-1.203 0.102z" /></svg>` },
+          link: "https://v.douyin.com/ij2Q7eeF"
+        }
+      ]
+    }
+  ]
+</script>
+
+<template>
+  <VPTeamPage>
+    <VPTeamPageTitle>
+      <template #title>
+        核心成员介绍
+      </template>
+    </VPTeamPageTitle>
+    <VPTeamMembers :members="members" />
+  </VPTeamPage>
+</template>
--- docs/index.md
+++ docs/index.md
@@ -7,7 +7,7 @@ titleTemplate: Front-end King
 hero:
   name: Cholez
   text: Front-end King
-  tagline: 我曾踏足山巅，也曾跌入低谷，二者都让我受益良多
+  tagline: 🌈 仰望星空的人，不应该被嘲笑
   
   image:
     src: /icon.png
@@ -29,17 +29,18 @@ features:
     link: http://docs.yuwenjian.com
     linkText: Cholez Web Docs
   
+  - icon: ✏️
+    title: 算法
+    details: 算法对于前端来说重要吗？思考这个问题的时间，不如直接开始吧。
+    link: /algorithm/
+    linkText: 每日 LeetCode
+  
   - icon: <svg xmlns="http://www.w3.org/2000/svg" width="30" viewBox="0 0 256 220.8"><path fill="#41B883" d="M204.8 0H256L128 220.8 0 0h97.92L128 51.2 157.44 0h47.36Z"/><path fill="#41B883" d="m0 0 128 220.8L256 0h-51.2L128 132.48 50.56 0H0Z"/><path fill="#35495E" d="M50.56 0 128 133.12 204.8 0h-47.36L128 51.2 97.92 0H50.56Z"/></svg>
     title: 深入解析 Vue.js
     details: 响应系统、渲染器、组件化、编译器究竟是如何实现的呢？
     link: /
     linkText: 解读《Vue.js 设计与实现》
   
-  - icon: ✏️
-    title: 算法
-    details: 算法对于前端来说重要吗？思考这个问题的时间，不如写一道算法题。
-    link: /
-    linkText: 每日 LeetCode
   
   - icon: 🏠
     title: 记录生活
@@ -49,37 +50,7 @@ features:
 ---
 
 <script setup>
-  import { VPTeamPage, VPTeamPageTitle, VPTeamMembers } from "vitepress/theme"
-  import icons from "./.vitepress/theme/icons/index.ts"
-  
-  const members = [
-    {
-      avatar: "https://www.github.com/CholezYu.png",
-      name: "Cholez.Yu",
-      title: "Front-end King",
-      desc: `🌈 我曾踏足山巅，也曾跌入低谷，二者都让我受益良多`,
-      links: [
-        { icon: "github", link: "https://github.com/CholezYu" },
-        { icon: { svg: icons.tiktok }, link: "https://v.douyin.com/i2THwY7e" },
-      ]
-    },
-    {
-      avatar: "https://p3-pc.douyinpic.com/img/aweme-avatar/tos-cn-i-0813_ooI98CkABJNjr6oonAEeWqeCLAAC0gbODHAAzz~c5_300x300.jpeg?from=2956013662",
-      name: "小丸子",
-      title: "清清不开心",
-      desc: `FE Developer <br /> My Honey`,
-      links: [
-        { icon: { svg: icons.tiktok }, link: "https://v.douyin.com/ij2Q7eeF" }
-      ]
-    }
-  ]
+  import Members from "./components/Members.vue"
 </script>
 
-<VPTeamPage>
-  <VPTeamPageTitle>
-    <template #title>
-      核心成员介绍
-    </template>
-  </VPTeamPageTitle>
-  <VPTeamMembers :members="members" />
-</VPTeamPage>
+<Members />
