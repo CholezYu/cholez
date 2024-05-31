5ac3e6f5413c5032dc58d39db9994ef80b717005
CholezYu <galaxy.nebula2021@gmail.com>
2024-06-01 01:26:05 +0800

    chore(sidebar): 算法

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -1,10 +1,28 @@
-const sidebar = [
-  {
-    text: "开始",
-    items: [
-      { text: "简介", link: "/intro" }
-    ]
-  }
-]
+import { DefaultTheme } from "vitepress"
+
+const sidebar: DefaultTheme.Sidebar = {
+  "/": [
+    {
+      text: "开始",
+      items: [
+        { text: "简介", link: "/intro" }
+      ]
+    }
+  ],
+  
+  "/algorithm/": [
+    {
+      text: "<b>为什么学习算法？</b>",
+      link: "/algorithm/"
+    },
+    {
+      text: "数组",
+      collapsed: false,
+      items: [
+        { text: "1.两数之和", link: "/algorithm/array-list/1.两数之和" }
+      ]
+    }
+  ]
+}
 
 export default sidebar
